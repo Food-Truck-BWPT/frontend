f296824146c7df041f50bff108a85ca00a3b84d0
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-06-17 20:47:32 -0700

    got the Header component set up

--- foodtrucker/src/components/Header.js
+++ foodtrucker/src/components/Header.js
@@ -1,1 +1,32 @@
-import react
+import React from "react"
+import styled from "styled-components"
+import {Link} from "react-router-dom"
+
+const StyledHeader = styled.header`
+
+    width: 100%;
+    background-color: #323643;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    height: 20vh;
+`
+
+function Header() {
+    return (
+        <StyledHeader>
+            <h1>Title</h1>
+            <form>
+                <label htmlFor="search">Search: </label>
+                <input name="search"></input>
+            </form>
+            <nav>
+                <Link>Home</Link>
+                <Link>Login</Link>
+                <Link>Sign-up</Link>
+            </nav>
+        </StyledHeader>
+    )
+}
+
+export default Header;
