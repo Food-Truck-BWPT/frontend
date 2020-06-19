34e5b8750abc0ca0e04b07d499ae9b43449bf4ab
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-06-18 18:40:31 -0700

    added app div

--- foodtrucker/src/App.js
+++ foodtrucker/src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import Header from "./components/Header"
+import Header from "./components/Header";
 
 function App() {
   return (
-    <Header />
+    <div className="App">
+      <Header />
+    </div>
   );
 }
 
