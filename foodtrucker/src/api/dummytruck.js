5dd8464b2c972e3541cb9ef70d8036590b44668c
James Leyden <jamesedward1024@gmail.com>
2020-06-23 20:30:08 -0500

    -

---
+++ foodtrucker/src/api/dummytruck.js
@@ -0,0 +1,17 @@
+{
+    "id": 1,
+    "name": "Mack's BBQ Truck",
+    "imageOfTruck": "image url/route",
+    "cuisineType": "Barbeque",
+    "customerRatings": "1234",
+    "customerRatingAvg": null,
+    "address": "123 Main. Street San Francisco, CA 94901",
+    "lat": "0",
+    "long": "1",
+    "departureTime": "4:30 PM",
+    "nex_address": "456 Last Street San Francisco, CA 94901",
+    "next_lat": "2",
+    "next_long": "3",
+    "next_arrivalTime": "11:00 AM",
+    "next_departureTime": "6:00 PM"
+}
