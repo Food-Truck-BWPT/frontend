0f6d18d11f0af648d1e16496852fe0f092f15afe
James Leyden <jamesedward1024@gmail.com>
2020-06-23 21:11:26 -0500

    profile ternary

--- foodtrucker/src/components/profile/Profile.js
+++ foodtrucker/src/components/profile/Profile.js
@@ -1,16 +1,28 @@
 import React from "react"
 import VendorNav from "./VendorNav"
 import NewFoodTruck from "./foodTruckForm"
+import { connect } from "react-redux"
 import {Route} from "react-router-dom"
 
 function Profile() {
+    const isVendor = false;
     return (
         <section>
-            <VendorNav />
-            <Route exact path="/profile/newtruck">
-                <NewFoodTruck />
-            </Route>
+            { isVendor ?
+                (
+                    <div>
+                    <VendorNav />
+                    <Route exact path="/profile/newtruck" component={NewFoodTruck} />
+
+                    </div>
+
+                ) :
+                (
+                    <div>user</div>
+                )}
+
         </section>
+
     )
 }
 
